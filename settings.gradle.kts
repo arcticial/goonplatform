53afae93b25b309e4dbb8d5abf94133a75ebd563
sammi <maltrolling@gmail.com>
2026-08-05 11:41:16 -0400

    buh

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,4 +7,6 @@ includeBuild("modules/module-eag-1_14")
 include("mixin:mixin-loader")
 include("mixin:mixins-1_5_2")
 include("mixin:mixins-1_8")
-include("mixin:mixins-1_14")
+
+project(":mixin:mixins-1_5_2").projectDir = file("mixin/type/eag/1_5_2")
+project(":mixin:mixins-1_8").projectDir = file("mixin/type/eag/1_8")
